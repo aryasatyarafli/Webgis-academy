145110d16521e83982768bf275f1bec7c49fc3f9
M Aryasatya Rafliando <rafliando@gmail.com>
2026-07-13 19:42:44 +0700

    add layer geojson with MAPID API

--- webapp-maplibre/src/main.js
+++ webapp-maplibre/src/main.js
@@ -9,16 +9,15 @@ document.body.appendChild(mapElement);
 const map = new Map({
     container: 'map',
     style: 'https://demotiles.maplibre.org/globe.json',
-    center: [106.89, -6.19],
-    zoom: 6,
+    center: [106.83, -6.19],
+    zoom: 8,
 });
-
 
 
 map.on('load', () => {
 map.addSource("Kota", {
     type: "geojson",
-    data: naturalEarthData
+    data: "https://geoserver.mapid.io/layers_new/get_layer?api_key=42a17dc7d53c49c3807ee35b6e56ee11&layer_id=6a54dbd27361c8b74fb8b74b&project_id=6a2d071bbccdad7e1b06220a"
 })
 
 map.addLayer({
